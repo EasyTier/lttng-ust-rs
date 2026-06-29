498df864bcd779f8faa87b6908743f2c4d6dcf02
fanyang <fanyang89@outlook.com>
2026-06-29 23:44:12 +0800

    Support Rust 2024 extern bindings

--- lttng-ust-generate/src/generator/rust_bindings.rs
+++ lttng-ust-generate/src/generator/rust_bindings.rs
@@ -3,13 +3,15 @@ use std::io;
 use std::io::prelude::*;
 use std::path::PathBuf;
 
-use ::{CTFType, EventClass, EventInstance, Field, Provider};
+use {CTFType, EventClass, EventInstance, Field, Provider};
 
 use super::tracepoint_interface::generate_func_name;
 
-pub(in super) fn generate_rust_bindings(output_path: &PathBuf,
-                                        providers: &[Provider],
-                                        raw_bindings: &PathBuf) -> io::Result<()> {
+pub(super) fn generate_rust_bindings(
+    output_path: &PathBuf,
+    providers: &[Provider],
+    raw_bindings: &PathBuf,
+) -> io::Result<()> {
     let mut outf = File::create(output_path)?;
     write_include(&mut outf, raw_bindings)?;
     write_providers(&mut outf, providers)?;
@@ -22,7 +24,12 @@ fn write_include<F: Write>(outf: &mut F, raw_bindings_path: &PathBuf) -> io::Res
     write!(outf, "#[allow(non_camel_case_types)]\n")?;
     write!(outf, "#[allow(non_snake_case)]\n")?;
     write!(outf, "mod detail {{\n")?;
-    io::copy(&mut File::open(raw_bindings_path)?, outf)?;
+    let bindings = std::fs::read_to_string(raw_bindings_path)?;
+    write!(
+        outf,
+        "{}",
+        bindings.replace("extern \"C\"", "unsafe extern \"C\"")
+    )?;
     write!(outf, "}}\n")?;
 
     Ok(())
@@ -42,63 +49,68 @@ fn write_providers<F: Write>(outf: &mut F, providers: &[Provider]) -> io::Result
     Ok(())
 }
 
-fn generate_instance_call(provider: &Provider, class: &EventClass, instance: &EventInstance) -> String {
+fn generate_instance_call(
+    provider: &Provider,
+    class: &EventClass,
+    instance: &EventInstance,
+) -> String {
     let name = &instance.name;
     let type_args = "";
-    let args: Vec<String> = class.fields.iter().enumerate()
-        .map(|(i, field)| {
-            format!("a{}: {}", i, rust_type_for(&field.ctf_type))
-        }).collect();
+    let args: Vec<String> = class
+        .fields
+        .iter()
+        .enumerate()
+        .map(|(i, field)| format!("a{}: {}", i, rust_type_for(&field.ctf_type)))
+        .collect();
     let args = &args.join(", ");
-    let c_args: Vec<String> = class.fields.iter().enumerate()
-        .map(|(i, field)| {
-            c_arg_for_field(format!("a{}", i), field)
-        }).collect();
+    let c_args: Vec<String> = class
+        .fields
+        .iter()
+        .enumerate()
+        .map(|(i, field)| c_arg_for_field(format!("a{}", i), field))
+        .collect();
     let c_args = &c_args.join(", ");
     let native_name = generate_func_name(provider, class, instance);
-    format!(r"
+    format!(
+        r"
         pub(in super::super) fn {}<{}>({}) {{
             unsafe {{
                 super::detail::{}({})
             }}
         }}
-", name, type_args, args, native_name, c_args)
+",
+        name, type_args, args, native_name, c_args
+    )
 }
 
 fn rust_type_for(ty: &CTFType) -> String {
     use CTFType::*;
     match *ty {
-        Integer(i) |
-        IntegerNoWrite(i) |
-        IntegerHex(i) |
-        IntegerNetwork(i) |
-        IntegerNetworkHex(i) => i.rust_type().into(),
+        Integer(i) | IntegerNoWrite(i) | IntegerHex(i) | IntegerNetwork(i)
+        | IntegerNetworkHex(i) => i.rust_type().into(),
 
-        Float(f) |
-        FloatNoWrite(f) => f.rust_type().into(),
+        Float(f) | FloatNoWrite(f) => f.rust_type().into(),
 
-        String |
-        StringNoWrite => "::std::ffi::CStr".into(),
+        String | StringNoWrite => "::std::ffi::CStr".into(),
 
-        Array(i, l) |
-        ArrayNoWrite(i, l) => format!("&[{}; {}]", i.rust_type(), l),
+        Array(i, l) | ArrayNoWrite(i, l) => format!("&[{}; {}]", i.rust_type(), l),
 
         ArrayText(_) => "&str".into(),
 
-        Sequence(i) |
-        SequenceNoWrite(i) => format!("&[{}]", i.rust_type()),
+        Sequence(i) | SequenceNoWrite(i) => format!("&[{}]", i.rust_type()),
 
-        SequenceText |
-        SequenceTextNoWrite => "&str".into(),
+        SequenceText | SequenceTextNoWrite => "&str".into(),
 
-        Enum |
-        EnumNoWrite => unimplemented!(),
+        Enum | EnumNoWrite => unimplemented!(),
     }
 }
 
 fn c_arg_for_field(base_name: String, field: &Field) -> String {
     if let CTFType::SequenceText = field.ctf_type {
-        format!("::std::mem::transmute({0}.as_bytes().as_ptr()), {0}.len()", base_name)
+        format!(
+            "::std::mem::transmute({0}.as_bytes().as_ptr()), {0}.len()",
+            base_name
+        )
     } else if field.ctf_type.is_sequence() {
         base_name.clone() + ", " + &base_name + ".len()"
     } else if let CTFType::Array(_, _) | CTFType::ArrayNoWrite(_, _) = field.ctf_type {
